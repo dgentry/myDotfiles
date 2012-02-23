e2ace8f214a98d96dd01e9654f93c37bc8a62a0e
Dennis Gentry <dennis.gentry@gmail.com>
2012-02-22 22:50:33 -0800

    Separated my python programming stuff into this file.

---
+++ dot-emacs.d/python-programming.el
@@ -0,0 +1,47 @@
+(setq load-path
+      (append (list nil
+		    "~/.emacs.d/python-mode-1.0/"
+		    "~/.emacs.d/pymacs/"
+		    "~/.emacs.d/ropemacs-0.6"
+		    )
+              load-path))
+(setq python-python-command "ipython")
+
+(defadvice py-execute-buffer (around python-keep-focus activate)
+  "return focus to python code buffer"
+  (save-excursion ad-do-it))
+
+(require 'ipython)
+(setq py-python-command-args '( "--colors" "Linux"))
+
+(require 'python-mode)
+
+(setenv "PYMACS_PYTHON" "python2.7") 
+(require 'pymacs)
+(setq pymacs-load-path '("~/.emacs.d/ropemode"
+			 "~/.emacs.d/ropemacs"))
+(pymacs-load "ropemacs" "rope-")
+
+
+(add-hook 'python-mode-hook
+          '(lambda () (eldoc-mode 1)) t)
+
+;
+; Make flymake work for python.  From 
+;   http://www.plope.com/Members/chrism/flymake-mode
+
+(when (load "flymake" t)
+  (defun flymake-pycheckers-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "~/.emacs.d/pycheckers.py" (list local-file))))
+
+  (add-to-list 'flymake-allowed-file-name-masks 
+	       '("\\.py\\'" flymake-pycheckers-init))) 
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
+
+(provide 'python-programming)
