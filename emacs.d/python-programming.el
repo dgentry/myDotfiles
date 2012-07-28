3df1807748cfc99447a9b78ee3f33413f2f6626c
Dennis Gentry <dennis.gentry@gmail.com>
2012-07-28 00:42:24 -0700

    emacs.d/python-programming.el

--- emacs.d/python-programming.el
+++ emacs.d/python-programming.el
@@ -1,3 +1,38 @@
+(require 'python)
+(defun python--add-debug-highlight ()
+  "Adds a highlighter for use by `python--pdb-breakpoint-string'"
+  (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
+ 
+(add-hook 'python-mode-hook 'python--add-debug-highlight)
+ 
+(defvar python--pdb-breakpoint-string "import pdb; pdb.set_trace() ## DEBUG ##"
+  "Python breakpoint string used by `python-insert-breakpoint'")
+ 
+(defun python-insert-breakpoint ()
+  "Inserts a python breakpoint using `pdb'"
+  (interactive)
+  (back-to-indentation)
+  ;; this preserves the correct indentation in case the line above
+  ;; point is a nested block
+  (split-line)
+  (insert python--pdb-breakpoint-string))
+(define-key python-mode-map (kbd "<f5>") 'python-insert-breakpoint)
+ 
+(defadvice compile (before ad-compile-smart activate)
+  "Advises `compile' so it sets the argument COMINT to t
+if breakpoints are present in `python-mode' files"
+  (when (derived-mode-p major-mode 'python-mode)
+    (save-excursion
+      (save-match-data
+        (goto-char (point-min))
+        (if (re-search-forward (concat "^\\s-*" python--pdb-breakpoint-string "$")
+                               (point-max) t)
+            ;; set COMINT argument to `t'.
+            (ad-set-arg 1 t))))))
+
+
+(add-hook 'shell-mode-hook 'compilation-shell-minor-mode)
+
 (setq load-path
       (append (list nil
 		    "~/.emacs.d/python-mode-1.0/"
