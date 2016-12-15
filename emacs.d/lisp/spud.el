7e38c334d66fbbaa82fc3a4950a6f4f528a830c7
Dennis Gentry <dennis.gentry@gmail.com>
2016-12-15 10:12:24 -0800

    Some stuff that I want, but don't have time to make work.
    Former-commit-id: 21379afdd5d04c563f5a31e8888f24bb2edcfe14

--- emacs.d/lisp/spud.el
+++ emacs.d/lisp/spud.el
@@ -124,4 +124,14 @@ display-time-filter  used by  display-time."
 
 (put 'eval-expression 'disabled nil)
 
+
+;(add-to-list compilation-error-regexp-alist '
+;	     (shellcheck "^In \\([^: \n	]+\\) line \\([0-9]+\\):" 1 2))
+
+; XTERM 256 color
+;(progn (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
+;       (setq comint-output-filter-functions (remove 'ansi-color-process-output comint-output-filter-functions))
+;       (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region))
+
+
 (provide 'spud)
