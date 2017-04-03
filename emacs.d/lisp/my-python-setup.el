ba6afd7d0e6bea426e4a57edfa42f75e749c3a67
Dennis Gentry <dennis.gentry@gmail.com>
2017-04-03 16:21:05 -0700

    Pymacs, apparently.
    Former-commit-id: 67d1f46a6c199967124cb96b6e4d59a97a0b705d

--- emacs.d/lisp/my-python-setup.el
+++ emacs.d/lisp/my-python-setup.el
@@ -19,7 +19,15 @@
 (push "~/.virtualenvs/v/bin" exec-path)
 (setenv "PATH" (concat "~/.virtualenvs/v/bin" ":" (getenv "PATH")))
 
+;; Pymacs
 (require 'pymacs)
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+
 (pymacs-load "ropemacs" "rope-")
 (setq ropemacs-enable-autoimport 't)
 ; add the name of modules you want to autoimport
