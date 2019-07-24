87ebe5c81fcdd154898b47d63f77c1a1e65867d8
Dennis Gentry <dennis.gentry@gmail.com>
2019-07-23 21:36:32 -0700

    Now with two systems.

--- install-git-town.sh
+++ install-git-town.sh
@@ -1,5 +1,17 @@
 !#/bin/bash
 
-# For 64 bit amd
-curl -O -L https://github.com/Originate/git-town/releases/download/v7.2.1/git-town-amd64.deb
-sudo dpkg -i git-town-amd64.deb
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    echo "Trying 64 bit amd."
+    if [[ -x `which dpkg` ]]; then
+	curl -O -L https://github.com/Originate/git-town/releases/download/v7.2.1/git-town-amd64.deb
+	sudo dpkg -i git-town-amd64.deb
+    else
+	echo "No dpkg.  Giving up."
+    fi
+    
+elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+    echo "Trying linux arm."
+    curl -O -L https://github.com/Originate/git-town/releases/download/v7.2.1/git-town-linux-arm
+    chmod +x git-town-linux-arm
+    sudo cp git-town-linux-arm /usr/local/bin/git-town
+fi
