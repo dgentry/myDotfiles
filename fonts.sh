d4cf8b874d5286a4ea8086ca1be7d2557b94c13b
Dennis Gentry <dennis.gentry@gmail.com>
2019-09-07 01:24:42 -0700

    Keep fonts here.

---
+++ fonts.sh
@@ -0,0 +1,3 @@
+[ -d /usr/share/fonts/opentype ] || sudo mkdir /usr/share/fonts/opentype
+sudo git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git /usr/share/fonts/opentype/scp
+sudo fc-cache -f -v
