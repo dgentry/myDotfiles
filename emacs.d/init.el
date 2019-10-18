c34b112411a0fe428f12677287cd15963c5973d1
Dennis Gentry <dennis.gentry@gmail.com>
2019-10-17 23:59:04 -0700

    No auto-package-update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -168,11 +168,11 @@ static char *gnus-pointer[] = {
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-(use-package auto-package-update
-             :config
-             (setq auto-package-update-delete-old-versions t)
-             (setq auto-package-update-hide-results t)
-             (auto-package-update-maybe))
+;(use-package auto-package-update
+;             :config
+;             (setq auto-package-update-delete-old-versions t)
+;             (setq auto-package-update-hide-results t)
+;             (auto-package-update-maybe))
 
 (use-package req-package
   :ensure t
