4b16b022d4fa7f0ec67cc5e760b640c9d683241e
Dennis Gentry <dennis.gentry@gmail.com>
2021-01-03 17:12:28 -0800

    Initial

---
+++ msg.sh
@@ -0,0 +1,17 @@
+# Variables for colors/contrast
+txtund=$(tput sgr 0 1)          # Underline
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) #  red
+bldblu=${txtbld}$(tput setaf 4) #  blue
+bldwht=${txtbld}$(tput setaf 7) #  white
+txtrst=$(tput sgr0)             # Reset
+info=${bldwht}*${txtrst}        # Feedback
+pass=${bldblu}*${txtrst}
+warn=${bldred}*${txtrst}
+ques=${bldblu}?${txtrst}
+
+# Provide our messages with our name, and contrast to the other
+# install messages that will be going by
+msg() {
+    echo "$bldblu${myname}: $1${txtrst}"
+}
