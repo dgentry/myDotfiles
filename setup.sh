7a29d97fb581e1924201c3aced8edc8e4178a089
Dennis Gentry <dennis.gentry@gmail.com>
2021-01-03 17:11:52 -0800

    Accumulate all installs

--- setup.sh
+++ setup.sh
@@ -1,42 +1,96 @@
 #!/usr/bin/env bash
 
+# So this can run by double-clicking this script, identify its path.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative
+  # to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+my_long_name=$0  # Something like "./setup.sh"
+annoying_prefix="./"
+myname=${my_long_name#$annoying_prefix}  # Now just "install-brew.sh"
+source msg.sh
+
+
 BASHCOMP=~/.bash_completion.d
 
 # Make a .bash_completion directory
-if [[ ! -d "$BASHCOMP" ]]; then
+if ! [[ -d "$BASHCOMP" ]]; then
     # If this ever turns out to be a symlink (which it could), I'll add the -L check.
     mkdir "$BASHCOMP"
 fi
 
-# Symlink .git_completion until I figure out how I really want to do multilevel symlinks.
-if [[ ! -L "$BASHCOMP/git_completion.bash" ]]; then
-    ln -s "$PWD/git-completion.bash" "$BASHCOMP/"
+# Symlink .git_completion if necessary
+GITCOMP=git-completion.bash
+BCSCRIPT=$BASHCOMP/$GITCOMP
+if ! [ -L $BCSCRIPT ]; then
+    msg "Setting up git completion for bash"
+    ln -s $PWD/$GITCOMP $BCSCRIPT
 fi
 
 # Turn off pager behavior for git
 git config --global pager.branch false
 
+function brew_needs_install() {
+    # First arg is --cask or ""
+    # If the package is missing, return true
+    if brew ls $1 --versions $2 > /dev/null 2>&1; then
+        return 1
+    fi
+    return 0
+}
+
+function fetch_deep() {
+    if $(git rev-parse --is-shallow-repository $1); then
+        msg "Unshallowing $1"
+        git -C $1 fetch --unshallow
+    fi
+}
+
 name="$(uname)"
 if [ $name == "Darwin" ]; then
-    echo "It's a Mac."
+    msg "I'm a Mac."
 
-    # Install brew, I guess
-    source install-brew.sh
+    # Install brew if necessary
+    if ! [ -x $(which brew) ]; then
+        msg "Installing brew"
+        source install-brew.sh
+    fi
 
-    # Apparently necessary due to some homebrew foul-up
-    git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core fetch --unshallow
-    git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask fetch --unshallow
-    brew install git-town
-    brew install --cask google-chrome
-    brew install --cask iterm2 slack discord
+    # Apparently necessary, just once, due to some homebrew foul-up
+    fetch_deep /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core
+    fetch_deep /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask
 
-    brew install --cask quicksilver caffeine
-    brew install --cask clover-configurator
-    brew install --cask synergy
-    brew install --cask steam battle-net
-    brew install --cask backblaze
-    brew install --cask macdown
-    brew install --cask vlc
+    # git-town is the only one not in "cask"
+    if brew_needs_install "" git-town; then
+        brew install git-town
+    fi
+
+    brew_wanted="google-chrome iterm2 slack discord quicksilver caffeine clover-configurator synergy steam battle-net macdown vlc"
+    brew_to_install=""
+
+    msg "Checking for previous brew installs of $brew_wanted"
+    msg "This can take a minute, but it saves time later."
+    # We have to check these one at a time because brew just errors
+    # out if you list one that isn't installed.
+    for pkg in $brew_wanted; do
+        if brew_needs_install --cask $pkg; then
+            brew_to_install="$brew_to_install $pkg"
+        fi
+    done
+
+    if [ $brew_to_install ]; then
+        # brew_to_install, if it has anything, has a leading space
+        msg "Installing$brew_to_install"
+        brew install --cask $brew_to_install
+    else
+        msg "Nothing to brew install"
+    fi
 
     defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 else
