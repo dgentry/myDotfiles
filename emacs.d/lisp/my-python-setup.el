409ba0ee78e82ce9cb7e88346ae5997deb95e3b8
Dennis Gentry <dennis.gentry@gmail.com>
2021-05-11 21:52:32 -0700

    Cleanup, switch to u-ctags, flycheck, remove pymacs

--- emacs.d/lisp/my-python-setup.el
+++ emacs.d/lisp/my-python-setup.el
@@ -3,90 +3,56 @@
 ;;;                    There are many like it, but this one is mine.
 ;;; Code:
 
+(use-package live-py-mode
+  :defer t)
+;; Called from use-package elpy's configure, so elpy stuff is available
+(elpy-enable)
+
 ; Should only be for python, and not sure I use this anymore.
 (global-set-key [C-c C-e] 'python-shell-send-buffer)
 
-(elpy-enable)
-; Maybe restore ipython once I get ipython working consistently
-; (elpy-use-ipython)
+;; Have elpy use Flycheck instead of flymake
+(when (load "flycheck" t t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Auto-format code on save
+(add-hook 'elpy-mode-hook (lambda ()
+                            (add-hook 'before-save-hook
+                                      'elpy-format-code nil t)))
+
+;; Maybe restore ipython once I get ipython working consistently
+;; (elpy-use-ipython)
 
 ;; Fixing a key binding bug in elpy
 ;(define-key yas-minor-mode-map (kbd "C-c k") 'yas-expand)
-;; Fixing another key binding bug in iedit mode
-(define-key global-map (kbd "C-c o") 'iedit-mode)
 
 ;; Hey, where did python-mode-map go?
 ;(define-key python-mode-map (kbd "C-c r m") 'python-insert-breakpoint)
 
-(push "~/.virtualenvs/v/bin" exec-path)
-(setenv "PATH" (concat "~/.virtualenvs/v/bin" ":" (getenv "PATH")))
-
-;; Pymacs
-;; (require 'pymacs)
-;; (autoload 'pymacs-apply "pymacs")
-;; (autoload 'pymacs-call "pymacs")
-;; (autoload 'pymacs-eval "pymacs" nil t)
-;; (autoload 'pymacs-exec "pymacs" nil t)
-;; (autoload 'pymacs-load "pymacs" nil t)
-;; (autoload 'pymacs-autoload "pymacs")
-
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport 't)
-;; ; add the name of modules you want to autoimport
-;; (setq ropemacs-autoimport-modules '("os" "shutil"))
+;; Use my most likely virtualenv
+(push "~/.virtualenvs/3/bin" exec-path)
+(setenv "PATH" (concat "~/.virtualenvs/3/bin" ":" (getenv "PATH")))
 
 (defcustom python-autopep8-path (executable-find "autopep8")
   "Autopep8 executable path."
   :group 'python
   :type 'string)
 
-; Probably want to switch to yapf
-(defun python-autopep8 ()
-  "Automatically formats Python code to conform to the PEP 8 style guide.
-$ autopep8 --in-place --aggressive <filename>"
-  (interactive)
-  (when (eq major-mode 'python-mode)
-    (shell-command
-     (format "%s --in-place --aggressive %s" python-autopep8-path
-             (shell-quote-argument (buffer-file-name))))
-    (revert-buffer t t t)))
+;; ; Probably just want to switch to yapf in elpy
+;; (defun python-autopep8 ()
+;;   "Automatically formats Python code to conform to the PEP 8 style guide.
+;; $ autopep8 --in-place --aggressive <filename>"
+;;   (interactive)
+;;   (when (eq major-mode 'python-mode)
+;;     (shell-command
+;;      (format "%s --in-place --aggressive %s" python-autopep8-path
+;;              (shell-quote-argument (buffer-file-name))))
+;;     (revert-buffer t t t)))
 
-(global-set-key "\C-c\C-a" 'python-autopep8)
 
-;(eval-after-load 'python
-;  '(if python-autopep8-path
-;       (add-hook 'before-save-hook 'python-autopep8)))
-
-(defmacro after (mode &rest body)
-  "Uh, MODE, BODY."
-  `(eval-after-load ,mode
-     '(progn ,@body)))
-
-(after 'auto-complete
-       (add-to-list 'ac-dictionary-directories "~/.emacs.d/dict")
-       (setq ac-use-menu-map t)
-       (define-key ac-menu-map "\C-n" 'ac-next)
-       (define-key ac-menu-map "\C-p" 'ac-previous))
-
-(after 'auto-complete-config
-       (ac-config-default)
-       (when (file-exists-p (expand-file-name "~/.emacs.d/lisp/Pymacs"))
-         (ac-ropemacs-initialize)
-         (ac-ropemacs-setup)))
-
-(after 'auto-complete-autoloads
-       (autoload 'auto-complete-mode "auto-complete" "enable auto-complete-mode" t nil)
-       (add-hook 'python-mode-hook
-                 (lambda ()
-                   (require 'auto-complete-config)
-                   (add-to-list 'ac-sources 'ac-source-ropemacs)
-                   (auto-complete-mode))))
-
-;; Not sure this section is compatible with pymacs/elpy.  It's a bit
-;; of support for interactive pdb in a compile buffer from
+;; support for interactive pdb in a compile buffer from
 ;; https://www.masteringemacs.org/article/compiling-running-scripts-emacs.
-;(require 'python)
-
 (defun python--add-debug-highlight ()
   "Add a highlighter for use by `python--pdb-breakpoint-string'."
   (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
@@ -116,12 +82,6 @@ $ autopep8 --in-place --aggressive <filename>"
                                (point-max) t)
             ;; set COMINT argument to `t'.
             (ad-set-arg 1 t))))))
-
-(use-package smart-compile)
-(use-package live-py-mode)
-(use-package flycheck
-  :hook (after-init . global-flycheck-mode))
-
 
 ; (flycheck-define-checker python-prospector
 ;   "A Python syntax and style checker using Prospector.
@@ -153,7 +113,7 @@ $ autopep8 --in-place --aggressive <filename>"
   "Create tags file in DIR-NAME."
   (interactive "DDirectory: ")
   (eshell-command
-   (format "find %s -type f -name \"*.[ch]\" | etags -" dir-name)))
+   (format "find %s -type f -name \"*.[ch]\" | ctags -" dir-name)))
 
 (defadvice find-tag (around refresh-etags activate)
   "Rerun etags and reload tags if tag not found.
@@ -178,24 +138,6 @@ Maybe EXTENSION is the extension type of files to run etags on."
 ;; Standard Jedi.el setting
 ;(add-hook 'python-mode-hook 'jedi:setup)
 ;(setq jedi:complete-on-dot t)
-
-(defun* get-closest-pathname (&optional (file "Makefile"))
-  "Determine the pathname of the first instance of FILE starting from the current directory towards root.  This may not do the correct thing in presence of links. If it does not find FILE, then it returns the name of FILE in the current directory, suitable for creation"
-    (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
-      (expand-file-name file
-                        (loop
-                         for d = default-directory then (expand-file-name ".." d)
-                         if (file-exists-p (expand-file-name file d))
-                         return d
-                         if (equal d root)
-                         return nil))))
-(require 'compile)
-(add-hook 'python-mode-hook
-          (lambda () (set (make-local-variable 'compile-command)
-                          (format "cd %s && make -k runrec"
-                                  (file-name-directory (get-closest-pathname))))))
-
-(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (provide 'my-python-setup)
 ;;; my-python-setup ends here
