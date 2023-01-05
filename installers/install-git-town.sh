3c2f7056cba46d33bb419f55f18dcb8f60ca4ab3
Dennis Gentry <dennis.gentry@gmail.com>
2023-01-04 16:22:38 -0800

    Works on Ubuntu 20.04 Intel 64

--- installers/install-git-town.sh
+++ installers/install-git-town.sh
@@ -1,17 +1,42 @@
-!#/bin/bash
+#!/bin/bash
+
+# I would check the version number, but git-town-version returns "Git Town  ()"
+git_town=$(which git-town)
+if [[ -x $git_town ]]; then
+    echo "$git_town appears to be installed, exiting."
+    exit
+fi
+
+# v7.8.0 current as of Jan 2023
+download_path="https://github.com/Originate/git-town/releases/download/v7.8.0"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    echo "Trying 64 bit amd."
+    echo "Trying 64 bit amd/intel."
+    filename="git-town_7.8.0_linux_intel_64.deb"
     if [[ -x `which dpkg` ]]; then
-	curl -O -L https://github.com/Originate/git-town/releases/download/v7.4.0/git-town-amd64.deb
-	sudo dpkg -i git-town-amd64.deb
+        if [[ ! -r ${filename} ]]; then
+            echo "Downloading ${filename}"
+	    curl -O -L "${download_path}/${filename}"
+        fi
+	sudo dpkg -i ${filename}
     else
 	echo "No dpkg.  Giving up."
     fi
 
 elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
     echo "Trying linux arm."
-    curl -O -L https://github.com/Originate/git-town/releases/download/v7.4.0/git-town-linux-arm
-    chmod +x git-town-linux-arm
-    sudo cp git-town-linux-arm /usr/local/bin/git-town
+    filename="git-town-linux-arm"
+    curl -O -L "${download_path}/${filename}"
+    chmod +x ${filename}
+    sudo cp ${filename} /usr/local/bin/git-town
+
+else
+    echo "Git town installer doesn't recognize this OS.  Giving up."
+fi
+
+# Remove download if git-town exists now
+git_town=$(which git-town)
+if [[ -x $git_town ]]; then
+    echo "$git_town is executable, deleting download"
+    \rm ${filename}
 fi
