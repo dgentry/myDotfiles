2532de01fede2f2df1fb4bad64d3119a561a2bb2
Dennis Gentry <dennis.gentry@gmail.com>
2023-01-15 13:24:30 +0000

    Update to 7.8.0

--- installers/install-git-town.sh
+++ installers/install-git-town.sh
@@ -1,17 +1,18 @@
 !#/bin/bash
 
+set -x
+
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Trying 64 bit amd."
     if [[ -x `which dpkg` ]]; then
-	curl -O -L https://github.com/Originate/git-town/releases/download/v7.4.0/git-town-amd64.deb
-	sudo dpkg -i git-town-amd64.deb
+	curl -O -L https://github.com/git-town/git-town/releases/download/v7.8.0/git-town_7.8.0_linux_intel_64.deb
+	sudo dpkg -i git-town_7.8.0_linux_intel_64.deb
     else
 	echo "No dpkg.  Giving up."
     fi
 
 elif [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
     echo "Trying linux arm."
-    curl -O -L https://github.com/Originate/git-town/releases/download/v7.4.0/git-town-linux-arm
-    chmod +x git-town-linux-arm
-    sudo cp git-town-linux-arm /usr/local/bin/git-town
+    curl -O -L https://github.com/git-town/git-town/releases/download/v7.8.0/git-town_7.8.0_linux_arm_64.deb
+    sudo dpkg -i git-town_7.8.0_linux_arm_64.deb
 fi
