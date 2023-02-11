a66a88e7a966c7963e855dfe3049278eb0193beb
Dennis Gentry <dennis.gentry@gmail.com>
2023-02-11 01:08:59 -0800

    What emacs 27 does to custom-settings.el, plus sort-region and zshenv

--- emacs.d/lisp/sort-region.el
+++ emacs.d/lisp/sort-region.el
@@ -10,3 +10,18 @@
     (goto-char sw-here)
     (sort-regexp-fields 'nil "[-a-zA-Z0-9]+" "\\&" (region-beginning) (region-end))
     (goto-char (region-end))))
+
+;; Or
+;; (defun sort-current-list ()
+;;   (save-mark-and-excursion
+;;     (search-backward "(")
+;;     (let ((beg (point))) (forward-list) ())
+;;     (setq bar (cl-sort fuu 'string<))))
+
+;;   "Sort the elements of list surrounding point"
+;;   (interactive)
+;;   (save-excursion (progn
+;;                     (backward-list)
+;;                     (set-point)
+;;                     (forward-list)
+;;                     (set-point)))))))
