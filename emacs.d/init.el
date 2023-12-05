bb1d2e58fa40026a81aa81315f496168db377aae
Dennis Gentry <dennis.gentry@gmail.com>
2023-12-04 20:23:01 -0800

    Add ellama!

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -974,6 +974,12 @@ Maybe EXTENSION is the extension type of files to run etags on."
 ;;   :defer t
 ;;   :bind (("C-x !" . gptel-send)))
 
+(use-package ellama
+  :init
+  (require 'llm-ollama)
+  (setopt ellama-provider
+ 	  (make-llm-ollama
+ 	   :chat-model "mistral" :embedding-model "mistral")))
 
 ;;(message (format "Gc-cons-threshold %d" start-gc-consthreshold))
 ;; Restore original, but x2
